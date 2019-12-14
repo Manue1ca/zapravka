54e3164a01d346f658d37e7fe18b102777e28059
Manue1ca <46681147+Manue1ca@users.noreply.github.com>
2019-12-14 19:48:38 +0900

    Update second.py

--- second.py
+++ second.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import sqlite3
+from tkinter import messagebox as mb
 def condb(event):
     conn = sqlite3.connect('db_kartridji.db')
     cursor = conn.cursor()
@@ -17,11 +18,20 @@ def condb(event):
         label3=Label(root,text='Не верный логин или пароль')
         label3.pack()
 
+def b1_main_event(event):
+    data_add = mb.adddata(title = 'Данные', message='Какие действие производились с картриджем?')
+
 def creat_main():
     root.destroy()
     root2 = Tk()
     root2.title('Главное окно')
     root2.geometry('600x800')
+    label_for_buttons=Label(root2, width=200, height=5)
+    label_for_buttons.config(bg='#938c8c')
+    label_for_buttons.pack()
+    b1_main = Button(text = 'Добавить', width=15, height=5)
+    b1_main.bind('Button-1', b1_main_event)
+    b1_main.pack()
 
 
 root = Tk()
