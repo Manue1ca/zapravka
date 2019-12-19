52d0ac65c363a5564fc3fe7c30ecfa4285bc8abf
Manue1ca <46681147+Manue1ca@users.noreply.github.com>
2019-12-19 17:27:58 +0900

    Update main.py

--- Secondary version/main.py
+++ Secondary version/main.py
@@ -18,46 +18,46 @@ def clear_text():
 def populate_list():
     return
 
-# Create window object
+# создаем окно
 app = Tk()
 
-# kart
+# картридж
 kart_text = StringVar()
 part_label = Label(app, text='Картридж', font=('bold', 14), pady=20)
 part_label.grid(row=0, column=0, sticky=W)
 part_entry = Entry(app, textvariable=kart_text)
 part_entry.grid(row=0, column=1)
-# Customer
+# тип
 type_text = StringVar()
 customer_label = Label(app, text='Тип', font=('bold', 14))
 customer_label.grid(row=0, column=2, sticky=W)
 customer_entry = Entry(app, textvariable=type_text)
 customer_entry.grid(row=0, column=3)
-# Retailer
+# тонер
 toner_text = StringVar()
 retailer_label = Label(app, text='Тонер', font=('bold', 14))
 retailer_label.grid(row=1, column=0, sticky=W)
 retailer_entry = Entry(app, textvariable=toner_text)
 retailer_entry.grid(row=1, column=1)
-# Price
+# организация
 fromthis_text = StringVar()
-price_label = Label(app, text='От куда картридж', font=('bold', 14))
+price_label = Label(app, text='Организация', font=('bold', 14))
 price_label.grid(row=1, column=2, sticky=W)
 price_entry = Entry(app, textvariable=fromthis_text)
 price_entry.grid(row=1, column=3)
 # Parts List (Listbox)
 parts_list = Listbox(app, height=8, width=50, border=0)
 parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
-# Create scrollbar
+# скролл
 scrollbar = Scrollbar(app)
 scrollbar.grid(row=3, column=3)
-# Set scroll to listbox
+# скролл в листбоксе
 parts_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=parts_list.yview)
-# Bind select
+# бинд select
 parts_list.bind('<<ListboxSelect>>', select_item)
 
-# Buttons
+# кнопки
 add_btn = Button(app, text='Добавить', width=12, command=add_item)
 add_btn.grid(row=2, column=0, pady=20)
 
